95ba273251fcbc7dabdc755ec575ca00f2aa53a6
Jaim <131182310+jaim-kia@users.noreply.github.com>
2024-03-04 17:58:30 +0800

    Removed unnecessary functions as there were classes that did that did the same thing

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,17 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Recipe, Ingredient
-
-def recipes(request):
-	recipes = Recipe.objects.all()
-	ctx = { "recipes:": recipe }
-	return render(request, 'recipes.html', ctx)
-
-def recipe(request, pk):
-	ingredients = Ingredient.objects.filter(recipe__recipe__name="Recipe {}".format(pk))
-	ctx = { "ingredients:": ingredients }
-	return render(request, 'recipe.html', ctx)
+from .models import Recipe
 
 class RecipeListView(ListView):
 	model = Recipe
