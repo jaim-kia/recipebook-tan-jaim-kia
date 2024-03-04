3a7e82287bd0129e88ac4e38115fdf9b31a1e946
Jaim <131182310+jaim-kia@users.noreply.github.com>
2024-03-04 15:41:21 +0800

    Added admin contents

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 
-from .models import Recipe
-# Register your models here.
+from .models import Recipe, RecipeIngredient, Ingredient
+
+class RecipeInline(admin.StackedInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeInline]
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
